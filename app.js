4d18dd6536ca89c9a28255aa27ab4569a9cb257b
Anass-Dr <anasdrissi77@gmail.com>
2024-09-23 20:21:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const apiRoutes = require("./routes/api");
+const webRoutes = require("./routes/web");
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", webRoutes);
+app.use("/api", apiRoutes);
+
+module.exports = app;
